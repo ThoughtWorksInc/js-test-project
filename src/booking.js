2e0cd5650be09beebf589d953cf790e0f7a8e857
Danilo Vitoriano <vitoriano08@gmail.com>
2021-01-21 13:01:52 -0300

    tests passed

--- src/booking.js
+++ src/booking.js
@@ -1,33 +1,123 @@
 'use strict'
 
-const clients = ['regular', 'reward']
 const hotels = [
     { name: 'Lakewood', rating: '3', amountWeek: 110, amountWeekReward: 80, amountWeekend: 90, amountWeekendReward: 80 },
-    { name: 'Bridgewood', rating: '4', amountWeek: 110, amountWeekReward: 80, amountWeekend: 90, amountWeekendReward: 80 },
-    { name: 'Ridgewood', rating: '5', amountWeek: 110, amountWeekReward: 80, amountWeekend: 90, amountWeekendReward: 80 }
+    { name: 'Bridgewood', rating: '4', amountWeek: 160, amountWeekReward: 110, amountWeekend: 60, amountWeekendReward: 50 },
+    { name: 'Ridgewood', rating: '5', amountWeek: 220, amountWeekReward: 100, amountWeekend: 150, amountWeekendReward: 40 }
 ]
-const lakewood = ['16Mar2009', '17Mar2009', '18Mar2009']
-const bridgewood = ['20Mar2009', '21Mar2009', '22Mar2009']
-const ridgewood = ['26Mar2009', '27Mar2009', '28Mar2009']
+
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
 
 module.exports = {
     adicionar: (client, datas) => {
-        const array2 = datas
-        const array2Sorted = array2.slice().sort();
-        const response = (arr) => arr.length === array2.length && arr.slice().sort().every(function(value, index) {
-            return value === array2Sorted[index]
-        });
 
-        if (client === clients[0] && response(lakewood)) {
-            return hotels[0].name
-        }
+        console.log("client => ", client)
 
-        if (client === clients[0] && response(bridgewood)) {
-            return hotels[1].name
-        }
+        let weekdays = [0,0]
+        let hotelsTriage = [0,0,0]
 
-        if (client === clients[1] && response(ridgewood)) {
-            return hotels[2].name
-        }
+        // descobre quantas das datas sao dias de semana ou fim de semana
+        datas.map(
+            data => {
+                var d = new Date(data);
+                var n = weekday[d.getDay()];
+                n === 'Saturday' || n === 'Sunday' ? weekdays[0]++ : weekdays[1]++
+            }
+        )
+        
+        // descobre o valor total pra cada hotel
+        hotels.map(
+            (hotel,index) => {
+                let weekd = 0
+                let weeke = 0
+                if(client === 'reward'){
+                    weekd = hotel.amountWeekReward * weekdays[1]
+                    weeke = hotel.amountWeekendReward * weekdays[0]
+                } else {
+                    weekd = hotel.amountWeek * weekdays[1]
+                    weeke = hotel.amountWeekend * weekdays[0]
+                }
+                hotelsTriage[index] = weeke + weekd
+            }
+        )
+
+        // descobre o menor valor entre os hoteis
+        let menorValor = hotelsTriage.reduce((a,b) => Math.min(a,b))        
+        console.log("menor valor entre os hoteis ", menorValor)
+
+        // descobre os hoteis com menor valor
+        let unicos = []
+        hotelsTriage.filter(
+            (item, index) => {
+                
+                console.log(item)
+                console.log(menorValor)
+                console.log("index ", index)
+                if(item === menorValor){
+                    console.log("indexou ", index)
+                    unicos.push(index)
+                }
+            }
+        )
+        console.log("hotels com menores valores => ", unicos)
+        
+        let highestRatingUnicos = []
+
+        hotels.forEach(
+            (item,index) => {
+                unicos.forEach(
+                    itemu => {
+                        if(index === itemu){
+                            console.log("item.name ", item.name)
+                            console.log("item.rating ", item.rating)
+                            highestRatingUnicos.push(item.rating)
+                        }
+                    }
+                )
+            }
+        )
+        console.log("highestRatingUnicos ", highestRatingUnicos)
+
+        // descobre o maior rating entre os hoteis de menor valor
+        let maiorRating = highestRatingUnicos.reduce((a,b) => Math.max(a,b))
+
+        console.log("maiorRating ", maiorRating)
+
+        // encontra o indice dos hoteis com menor valor e maior rating
+        let hoteisRetornados = []
+        highestRatingUnicos.filter(
+            (rating, index) => {
+                if(rating === maiorRating.toString()){
+                    hoteisRetornados.push(unicos[index])
+                }
+            }
+        )
+        
+        console.log("hoteis com menor valor e maior rating ", hoteisRetornados)
+
+        let finalname = ''
+        hotels.filter(
+            (hotel, index) => {
+                hoteisRetornados.filter(
+                    (h,i) => {
+                        if(index === h){
+                            console.log("a porra do hotel ", hotel.name)
+                            finalname = hotel.name
+                        }
+                    }
+                )
+            }
+        )
+
+        return finalname
     }
 }
+
+// export default Entrada;
--- test/failing-test-spec.js
+++ test/failing-test-spec.js
@@ -4,25 +4,38 @@ const chai = require('chai')
 const Entrada =  require('../src/booking')
 const expect = chai.expect    
 const hotels = [
-  { name: 'Lakewood', rating: '3', amountWeek: 110, amountWeekReward: 80, amountWeekend: 90, amountWeekendReward: 80},
-  { name: 'Bridgewood', rating: '4', amountWeek: 110, amountWeekReward: 80, amountWeekend: 90, amountWeekendReward: 80},
-  { name: 'Ridgewood', rating: '5', amountWeek: 110, amountWeekReward: 80, amountWeekend: 90, amountWeekendReward: 80}
+  { name: 'Lakewood', rating: '3', amountWeek: 110, amountWeekReward: 80, amountWeekend: 90, amountWeekendReward: 80 },
+    { name: 'Bridgewood', rating: '4', amountWeek: 160, amountWeekReward: 110, amountWeekend: 60, amountWeekendReward: 50 },
+    { name: 'Ridgewood', rating: '5', amountWeek: 220, amountWeekReward: 100, amountWeekend: 150, amountWeekendReward: 40 }
 ]
+var d = new Date('16Mar2009');
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+
 
 describe('Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)', function () {
-    it('Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)', function () {
-      const hotel = Entrada.adicionar('regular', ['16Mar2009', '17Mar2009', '18Mar2009'])
-      expect(hotel).to.equal(hotels[0].name);
-    })
+  it('Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)', function () {
+    const hotel = Entrada.adicionar('regular', ['16Mar2009', '17Mar2009', '18Mar2009'])
+    expect(hotel).to.equal(hotels[0].name);
+  })
 })
 
 describe('Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)', function () {
-  it('should return Bridgewood', function () {
+  it('Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)', function () {
     const hotel = Entrada.adicionar('regular', ['20Mar2009', '21Mar2009', '22Mar2009'])
     expect(hotel).to.equal(hotels[1].name);
   })
-
 })
+
+
 
 describe('Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)', function () {
   it('should return Ridgewood', function () {
